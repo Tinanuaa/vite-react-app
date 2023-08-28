41d82041e5e8ac64bbda43345ab6e76aea05c764
Tingting Feng <fengt@ansto.gov.au>
2023-08-28 10:48:59 +1000

    get itk-vtk-viewer working

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,23 @@ import { Box, Typography, Grid, Divider,Stack  } from "@mui/material";
  
 function App() {
   const liveViewerRef = useRef(null);
-  const image =  new URL(
-    "https://data.kitware.com/api/v1/file/564a65d58d777f7522dbfb61/download/data.nrrd"
-  )  
+  // const image =  new URL(
+  //   "https://data.kitware.com/api/v1/file/564a65d58d777f7522dbfb61/download/data.nrrd"
+  // )  
+    const data =  (new Uint16Array(1024 * 1024 )).map(() => Math.random() * 100);
+ 
+  const image= { "imageType": {
+     "dimension": 2,
+     "componentType": "uint16",
+     "pixelType":  "Scalar",
+     "components": 1    },
+    "name": "test-data",
+    "origin": [0,0],
+    "spacing": [2,2],
+    "direction": [1,0,0,0,1,0,0,0,1],
+    "size": [1024,1024],
+    "data":data }
+
   useEffect(() => {
  
     console.log('itk', !!itkVtkViewer, itkVtkViewer);
@@ -18,7 +32,7 @@ function App() {
         {
           image: image,
           rotate: false,
-          // use2D: true,
+          use2D: true,
         });
     }
   }, []);
@@ -34,16 +48,3 @@ function App() {
 }
 
 export default App
-  // const data =  (new Uint16Array(1024 * 1024 )).map(() => Math.random() * 100);
- 
-  // const image= { "imageType": {
-  //    "dimension": 2,
-  //    "componentType": "uint16",
-  //    "pixelType":  "Scalar",
-  //    "components": 1    },
-  //   "name": "test-data",
-  //   "origin": [0,0],
-  //   "spacing": [2,2],
-  //   "direction": [1,0,0,0,0,1,0,1,0],
-  //   "size": [1024,1024],
-  //   "data":data }
